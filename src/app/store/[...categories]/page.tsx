d112b81fc2586744275de8fc0cfe732d2fe2ad4d
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2024-02-26 22:14:17 -0500

    cambios

--- src/app/store/[...categories]/page.tsx
+++ src/app/store/[...categories]/page.tsx
@@ -10,7 +10,6 @@ interface CategoryProps {
 }
 
 export default function Category(props: CategoryProps) {
-    console.log(props)
     const { categories } = props.params
     return(
         <h1>Categoria dinámica: {categories}</h1>
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./MainProducts.module.sass";
+import { getProducts } from "app/services/shopify";
 
 interface Product {
   id: number;
@@ -34,35 +35,16 @@ interface Product {
   };
 }
 
-
-const getProducts = async () => {
-  try {
-    const response = await fetch(
-      `${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`,
-      {
-        headers: new Headers({
-          "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
-        }),
-      }
-    );
-    throw new Error("Error")
-    const {products} = await response.json();
-    return products;
-  } catch (error) {
-    console.log(error)
-  }
-  
-};
-
 export const MainProducts = async () => {
-  const products = await getProducts();
+  const response = await fetch("http://localhost:3000/api");
+  const {products} = await response.json()
   console.log("🚀 ~ MainProducts ~ products:", products);
 
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products.map((product) => {
+        {products?.map((product) => {
           const imageSrc = product.images[0].src;
           return (
             <article key={product.id}>
